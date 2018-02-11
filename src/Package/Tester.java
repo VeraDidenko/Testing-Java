c454fa11689ec88240a745f2e42e79dc40ad9db9
VeraDidenko <31995385+VeraDidenko@users.noreply.github.com>
2018-02-12 00:29:44 +0200

    Added more text
    Two lines :-)

--- src/Package/Tester.java
+++ src/Package/Tester.java
@@ -2,6 +2,7 @@ package Package;
 
 public class Tester {
 	public static void main(String [] args){
-		System.out.println("Hello! It project was created for you:)");
+		System.out.println("Hello! This project was created for you:)");
+		System.out.println("Enjoy!!! :-) ");
 	}
 }
